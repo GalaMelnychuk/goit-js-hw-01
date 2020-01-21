b97e2e447dc2e837c168671bafb104119c21cf12
Галина <galocka@MacBook-Galina.local>
2020-01-21 17:28:38 +0200

    after checking

--- task-2.js
+++ task-2.js
@@ -6,6 +6,6 @@ ordered = Number(ordered);
 
 if (ordered > total) {
   console.log(`На складе недостаточно товаров!`);
-} else if (ordered < total) {
+} else if (ordered <= total) {
   console.log(`Заказ оформлен, с вами свяжется менеджер`);
 }
